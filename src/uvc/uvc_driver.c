2c18610d7514b30a7ff6f60a32f1c5a1edde5ef2
Prabhat Maurya <prabhat@e.email>
2024-04-28 12:49:21 +0530

    upstream driver form kernel-6.8.7-300.fc40

--- src/uvc/uvc_driver.c
+++ src/uvc/uvc_driver.c
@@ -2454,8 +2454,8 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= UVC_PC_PROTOCOL_15,
 	  .driver_info		= (kernel_ulong_t)&uvc_ctrl_power_line_limited },
-
-	  /* Quanta ACER HD User Facing  0x4035 - Experimental */
+	  
+	/* Quanta ACER HD User Facing  0x4035 - Experimental */
 	{ .match_flags 	= USB_DEVICE_ID_MATCH_DEVICE
 			| USB_DEVICE_ID_MATCH_INT_INFO,
 	  .idVendor = 0x0408,
@@ -2465,7 +2465,7 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceProtocol =	UVC_PC_PROTOCOL_15,
 	  .driver_info = (kernel_ulong_t) &(const struct uvc_device_info ) {
 										.uvc_version = 0x010a, } },
-										
+
 	/* Quanta ACER HD User Facing 4033 - Experimental !! */
 	{ .match_flags 	= USB_DEVICE_ID_MATCH_DEVICE
 			| USB_DEVICE_ID_MATCH_INT_INFO,
@@ -2475,8 +2475,7 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass = 1,
 	  .bInterfaceProtocol =	UVC_PC_PROTOCOL_15,
 	  .driver_info = (kernel_ulong_t) &(const struct uvc_device_info ) {
-										.uvc_version = 0x010a, } },	  
-										
+										.uvc_version = 0x010a, } },
 	/* LogiLink Wireless Webcam */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
 				| USB_DEVICE_ID_MATCH_INT_INFO,
@@ -2615,6 +2614,15 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= 0,
 	  .driver_info		= (kernel_ulong_t)&uvc_ctrl_power_line_limited },
+	/* Chicony Electronics Co., Ltd Integrated Camera */
+	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
+				| USB_DEVICE_ID_MATCH_INT_INFO,
+	  .idVendor		= 0x04f2,
+	  .idProduct		= 0xb67c,
+	  .bInterfaceClass	= USB_CLASS_VIDEO,
+	  .bInterfaceSubClass	= 1,
+	  .bInterfaceProtocol	= UVC_PC_PROTOCOL_15,
+	  .driver_info		= (kernel_ulong_t)&uvc_ctrl_power_line_uvc11 },
 	/* Chicony EasyCamera */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
 				| USB_DEVICE_ID_MATCH_INT_INFO,
@@ -3017,6 +3025,15 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= 0,
 	  .driver_info		= UVC_INFO_QUIRK(UVC_QUIRK_FORCE_BPP) },
+	/* SunplusIT Inc HD Camera */
+	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
+				| USB_DEVICE_ID_MATCH_INT_INFO,
+	  .idVendor		= 0x2b7e,
+	  .idProduct		= 0xb752,
+	  .bInterfaceClass	= USB_CLASS_VIDEO,
+	  .bInterfaceSubClass	= 1,
+	  .bInterfaceProtocol	= UVC_PC_PROTOCOL_15,
+	  .driver_info		= (kernel_ulong_t)&uvc_ctrl_power_line_uvc11 },
 	/* Lenovo Integrated Camera */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
 				| USB_DEVICE_ID_MATCH_INT_INFO,
